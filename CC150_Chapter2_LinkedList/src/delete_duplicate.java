fc357c33b5ac3143f51274ec9f29f6a361773910
jish6303 <jiaju.shen@colorado.edu>
2014-11-07 23:42:40 -0500

    Question 1 of Chapter 2

--- CC150_Chapter2_LinkedList/src/delete_duplicate.java
+++ CC150_Chapter2_LinkedList/src/delete_duplicate.java
@@ -1,4 +1,43 @@
+import java.util.Hashtable;
 import java.util.Scanner;
 public class delete_duplicate {
+	public static void main(String[] args){
+		LinkedList<String> list1 = new LinkedList<String>();
+		System.out.println("Please enter the size of the LinkedList");
+	    Scanner input = new Scanner(System.in);
+	    int n = input.nextInt();
+	    String node;
+	    for(int i=0;i<=n;i++)
+	    {
+	    	node = input.nextLine();
+	    	list1.addLast(node);
+	    }
+	    System.out.println("Before Removing:");
+	    list1.print();
+	    delete_duplicate(list1);
+	}
 
+	private static LinkedList<String> delete_duplicate(LinkedList<String> list1) {	
+		Hashtable table = new Hashtable();
+		boolean remove=false;
+		LinkedList<String>.LinkedListNode<String> previous = list1.getfirst();
+		LinkedList<String>.LinkedListNode<String> cursor = list1.getfirst();
+		while(cursor.next!=null){
+			if(table.containsKey(cursor.data)){
+				remove=true;
+				previous.next=cursor.next;
+			}
+			else{
+				table.put(cursor.data, true);
+				previous=cursor;
+			}
+			cursor=cursor.next;
+		}
+		if(remove==true) {
+			System.out.println("After Removing:");
+			list1.print();
+		}
+		else System.out.println("No need to remove");
+		return list1;
+	}
 }
