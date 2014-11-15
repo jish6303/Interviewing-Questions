9411af3d5c7059119633436eb7aff0d2d8e13422
jish6303 <jiaju.shen@colorado.edu>
2014-11-14 21:51:07 -0500

    added another package of chapter 3.3, since there are too many files

---
+++ CC150_Chapter3_StackQueue/src/question3_3/Node.java
@@ -0,0 +1,11 @@
+package question3_3;
+//Code from Github of Gayle McDowell
+//https://github.com/gaylemcd
+public class Node {
+	public Node above;
+	public Node below;
+	public int value;
+	public Node(int value) {
+		this.value = value;
+	}
+}
