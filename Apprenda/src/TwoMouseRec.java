88c1201dd54632a81ab55d450080f248e5c2fc74
jish6303 <jiaju.shen@colorado.edu>
2014-11-22 22:20:01 -0500

    requirement added

--- Apprenda/src/TwoMouseRec.java
+++ Apprenda/src/TwoMouseRec.java
@@ -1,3 +1,42 @@
+/*
+ Problem Description:
+
+You are required to write code in the language of your choice implementing certain algorithms that 
+
+analyze rectangles and features that exist among rectangles. Your implementation is required to cover 
+
+the following:
+
+1. Intersection: You must be able to determine whether two rectangles have one or more 
+
+intersecting lines and produce a result identifying the points of intersection. For your 
+
+convenience, the scenario is diagrammed in Appendix 1.
+
+2. Containment: You must be able to determine whether a rectangle is wholly contained within 
+
+another rectangle. For your convenience, the scenario is diagrammed in Appendix 2.
+
+3. Adjacency: Implement the ability to detect whether two rectangles are adjacent. Adjacency is 
+
+defined as the sharing of a side. Side sharing may be proper or sub-line, where a sub-line share 
+
+is one where one side of rectangle A is a line that exists as a set of points wholly contained on 
+
+some other side of rectangle B. For your convenience, the scenario is diagrammed in Appendix 
+
+3.
+
+Your implementation should capture the rectangle entity as well as the operations listed above. Feel 
+
+free to expand on this problem as you wish. Document any expansion and provide it as part of your 
+
+submission.
+
+Your submitted source code must compile (if necessary) and the resulting executable must run on the 
+
+Windows platform. Please document any library or framework dependencies.
+ */
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Rectangle;
